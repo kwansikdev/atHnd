812e4fdb238dc2b4ed32a8fec591efc1875fd7fd
kwansikdev <kwansikdev@gmail.com>
2025-04-22 00:38:17 +0900

    📦 Add nanoid & shadcn-ui

---
+++ app/utils/generate-id.ts
@@ -0,0 +1,7 @@
+import { customAlphabet } from "nanoid";
+
+// 7-character random string
+export const shortId = customAlphabet(
+  "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz",
+  7
+);
